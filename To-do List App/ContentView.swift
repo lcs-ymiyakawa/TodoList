c89ba3d2a984bcbd9a0762973a67eafbe77f91c2
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-04-09 11:15:28 -0400

    Initial Commit

---
+++ To-do List App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-do List App
+//
+//  Created by 宮川義之助 on 2024/04/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To-do List App/To_do_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_List_AppApp.swift
+//  To-do List App
+//
+//  Created by 宮川義之助 on 2024/04/09.
+//
+
+import SwiftUI
+
+@main
+struct To_do_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
